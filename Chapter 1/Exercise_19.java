decd2b368a9209ff9fff41b4c02e3334031d6071
404kevnotfound <student@CSiMac1-10.local>
2018-12-12 10:35:36 -0700

    dvd

---
+++ Chapter 1/Exercise_19.java
@@ -0,0 +1,5 @@
+public class Exercise_19 {
+	public static void main(String[] args) {
+		System.out.println("Area = " + (4.5 * 7.9));
+		System.out.println("Perimeter = " + (4.5 + 7.9 + 4.5 + 7.9));	}
+}
